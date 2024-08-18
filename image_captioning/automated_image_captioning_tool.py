a871de86a0d72c3b756621d3dffd11f6a0920a98
Miracle Adebunmi <blueprime91@gmail.com>
2024-08-18 20:56:21 +0100

    Add local support

--- image_captioning/automated_image_captioning_tool.py
+++ image_captioning/automated_image_captioning_tool.py
@@ -1,4 +1,5 @@
 import glob
+import os
 import requests
 import gradio as gr
 from PIL import Image
@@ -6,48 +7,54 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 from transformers import AutoProcessor, BlipForConditionalGeneration
 
+from image_captioning.intro import img_url
 
 MODEL_BASE_URL = 'Salesforce/blip-image-captioning-base'
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif']
+DEFAULT_CUT_OFF = '400'
 
 processor = AutoProcessor.from_pretrained(MODEL_BASE_URL)
 model = BlipForConditionalGeneration.from_pretrained(MODEL_BASE_URL)
 
 
-
-def auto_captor(url, file_path, ext, progress=gr.Progress()) -> str:
-    progress(0, 'Starting...')
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    img_elements = soup.find_all('img')
-    status = []
-    with (open(file_path, 'w') as fp):
+def image_generator(url, ext:str, progress):
+    if url.startswith('http'):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        img_elements = soup.find_all('img')
         for img in progress.tqdm(img_elements):
             img_url = img['src']
-
-            # Correct the image URL if it's malformed
             if img_url.startswith("//"):
                 img_url = "https:" + img_url
 
-        # Skip if the image is an SVG or too small (likely an icon) or skip invalid URLs or not in selected extensions
-            if 'svg' in img_url or "1x1" in img_url or\
-                not (img_url.startswith('http://') or img_url.startswith('https://')) or \
-                    not img_url.split('.')[-1] in ext:
-                continue
+            # Skip if the image is an SVG or too small (likely an icon) or skip invalid URLs or not in selected extensions
+            if not ('svg' in img_url or "1x1" in img_url) and img_url.startswith('http') and img_url.split('.')[-1] in ext:
+                response = requests.get(img_url)
+                yield img_url, Image.open(BytesIO(response.content))
+    else:
+        for _ext in ext:
+            for paths in progress.tqdm(glob.glob(os.path.join(url, f'*.{_ext}'))):
+                yield paths, Image.open(paths)
+
+def auto_captor(url:str, file_path:str, ext:str, cut_off:str=DEFAULT_CUT_OFF, progress=gr.Progress()) -> str:
+    progress(0, 'Starting...')
+
+    status = []
+    with (open(file_path, 'w') as fp):
+        for img_path, image in image_generator(url, ext, progress):
 
             try:
-                response = requests.get(img_url)
-                raw_image = Image.open(BytesIO(response.content))
-                if raw_image.size[0] * raw_image.size[1] < 400: # Skip very small images
+
+                if image.size[0] * image.size[1] < int(cut_off): # Skip images which resolution is less than cut-off
                     continue
 
-                raw_image = raw_image.convert('RGB')
-                inputs = processor(raw_image, return_tensors='pt')
+                image = image.convert('RGB')
+                inputs = processor(image, return_tensors='pt')
                 outputs = model.generate(**inputs)
                 caption = processor.decode(outputs[0], skip_special_tokens=True)
-                fp.write("%s: %s\n" % (img_url, caption))
+                fp.write("%s: %s\n" % (img_path, caption))
             except Exception as e:
-                status.append("Error processing image %s: %s" % (img_url, str(e)))
+                status.append("Error processing image %s: %s" % (img_path, str(e)))
     return not status and "Success" or "Error: \n %s" % '\n'.join(errors for errors in status)
 
 UI = gr.Interface(
@@ -55,7 +62,9 @@ UI = gr.Interface(
     inputs=[
         "text",
         gr.File( file_count="single", file_types=[".txt"]),
-        gr.CheckboxGroup(ALLOWED_EXTENSIONS, label='Image Extensions', info="Select extension to track")],
+        gr.CheckboxGroup(ALLOWED_EXTENSIONS, label='Image Extensions', info="Select extension to track"),
+        gr.Text(lines=1, placeholder="Resolution Cut-off(integer) of images; Default is 400px", value='400'),
+    ],
     outputs="text",
     title="Automated Image Captioning Tool",
     description="Enter an valid url for automatic image captioning.",
